dc94b29c75ff524837147bc195d7f2441b89751a
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-18 10:27:33 +0100

    Refactored code to check if user id exists.

--- app.js
+++ app.js
@@ -7,12 +7,34 @@ function User(name, email, password) {
 
   if (this.constructor === User) {
     this.type = "User";
-  } else if (this.constructor === "Admin") {
+  } else if (this.constructor === Admin) {
     this.type = "Admin";
   }
+
+  for (var i = 1; i >= 0; ) {
+    if (!(db.Users[i + ""])) {
+      id = i + "";
+      db.Users[id] = {
+        name: this.name.toString(),
+        email: this.email.toString(),
+        password: this.password,
+        type: this.type.toString(),
+        id: id
+      };
+      break;
+    }
+    i++;
+  }
+  this.id = id;
+
+  
+  console.log(db);
 }
 
-function Admin (name, email, password) {
+User.prototype.createUserId = function(id) {};
+
+
+function Admin(name, email, password) {
   User.apply(this, [name, email, password]);
 
   Admin.prototype.constructor = Admin;
