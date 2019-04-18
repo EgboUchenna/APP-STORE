7298e3da5afe0596d26bb9d945af89ff45ae7ff1
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-18 12:34:27 +0100

    Added a test to read a single user by id

--- app.test.js
+++ app.test.js
@@ -1,7 +1,11 @@
 const { User, Admin } = require("./app");
+const db = require('./database');
+
 
 var Uchenna = new User("Uchenna", "egbouchenna001@gmail.com", 12345);
 var Matthew = new Admin("Matthew", "Uchesco4jesus@gmail.com", 54321);
+var Uche = new User("Uche", "uche4ma2w@gmail.com", 99887);
+var Muse = new Admin("Muse", "handsumMuse@gmail.com", 77889);
 
 test("User name to be name of the User", function() {
   expect(Uchenna.name).toBe("Uchenna");
@@ -22,3 +26,12 @@ test("User to have a password", function() {
 test('User type to be admin', function() {
   expect(Matthew.type).toBe('Admin');
 });
+
+test('Read User by Id to return a Single User', function() {
+  console.log(db);
+  expect(db.Users['1'].name).toBe('Uchenna');
+});
+
+
+
+
