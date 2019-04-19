da0ffec8b88a306aa762e17396e5a266d795ba2c
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-19 05:01:22 +0100

    Modified methods and refactored functions to pass two test cases of search User or admin by Name and return the Object.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const db = require("./database");
 
+//      ***************************     CREATE USER   ********************
 function User(name, email, password) {
   this.name = name;
   this.email = email;
@@ -28,12 +29,25 @@ function User(name, email, password) {
   this.id = id;
 }
 
+//  ********************** READ A SINGLE USER BY HIS ID  **********************
+
 User.prototype.readSingleUser = function(id) {
+  console.log(db.Users[id.toString()]);
   return db.Users[id.toString()];
 };
 
+//  ***************************  READ ALL USERS    ***************************
+
+Admin.prototype.readAllUsers = function() {
+  console.log(db.Users);
+  return db.Users;
+};
+
+//  *********************** UPDATE DETAILS OF A USER  ************************
+
 User.prototype.updateSingleUser = function(id, name, email, password) {
   if (!id || !name || !email || !password) {
+    console.log("All fields are required");
     return "All fields are required";
   }
   if (id.toString() !== this.id) {
@@ -42,8 +56,24 @@ User.prototype.updateSingleUser = function(id, name, email, password) {
   db.Users[id.toString()].name = name;
   db.Users[id.toString()].email = email;
   db.Users[id.toString()].password = password;
+  console.log("User updated successfully");
   return "User updated successfully";
 };
+
+//              ********************************  SEARCH USER BY NAME  ******************************
+
+User.prototype.searchUserByName = function(name) {
+  for (var userName in db.Users) {
+    if (db.Users[userName].name == name) {
+      console.log(db.Users[userName].name);
+      return db.Users[userName];
+    }
+  }
+  console.log("User does not Exist");
+  return false;
+};
+
+//        +++++++++++++++++++++++++++++++ ADMIN FUNCTIONALITIES  ++++++++++++++++++++++++++++++++++++++
 
 function Admin(name, email, password) {
   User.apply(this, [name, email, password]);
@@ -52,20 +82,23 @@ function Admin(name, email, password) {
 Admin.prototype = Object.create(User.prototype);
 Admin.prototype.constructor = Admin;
 
-Admin.prototype.readAllUsers = function() {
-  return db.Users;
-};
+//              ********************************  DELETE A SINGLE USER   ************************************
 
 Admin.prototype.deleteUser = function(id) {
   if (!db.Users[id.toString()]) {
+    console.log( "User not found");
     return "User not found";
   }
   delete db.Users[id.toString()];
+  console.log("User deleted sucessfully");
   return "User deleted sucessfully";
 };
 
+//              ********************************   DELETE ALL USERS  ****************************************
+
 Admin.prototype.deleteAllUsers = function() {
   db.Users = {};
+  console.log("All users deleted successfully");
   return "All Users deleted successfully";
 };
 
