cd88e8f1af68c3b1ef489a5ec3ac9d97ff264939
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-19 04:59:08 +0100

    Modified tests and Added two test cases to search User or admin by Name and return the Object.

--- app.test.js
+++ app.test.js
@@ -4,63 +4,72 @@ const db = require("./database");
 var User1 = new User("Uchenna", "egbouchenna001@gmail.com", 12345);
 var Admin1 = new Admin("Matthew", "Uchesco4jesus@gmail.com", 54321);
 var User2 = new User("Uche", "uche4ma2w@gmail.com", 99887);
-var Admin2 = new Admin("Mhdcb", "hdnksbas@gmail.com", "hjsdkjdj5648792nbmnb");
-var delUser = new User("delUser", "delUser@gmail.com", 9988700);
+var Admin2 = new Admin("Muse", "handsum-Muse@gmail.com", "334562");
+var User3 = new User("deleteUser", "deletedUser@gmail.com", "12345");
+var Admin3 = new Admin("Stephan", "rickraggardy@gmail.com", 28290);
 
 test("User name to be name of the User", function() {
   expect(User1.name).toBe("Uchenna");
 });
 
 test("User to have a Unique Id", function() {
-  expect(User1.id).toBeTruthy();
+  expect(User3.id).toBeTruthy();
 });
 
 test("User email to be email of the User", function() {
-  expect(User1.email).toBe("egbouchenna001@gmail.com");
+  expect(Admin3.email).toBe("rickraggardy@gmail.com");
 });
 
 test("User to have a password", function() {
-  expect(User1.password).toBeTruthy();
+  expect(User3.password).toBe("12345");
 });
 
-test("User type to be admin", function() {
+test(" User Type of to be Admin", function() {
   expect(Admin1.type).toBe("Admin");
 });
 
+test("User Type of to be User", function() {
+  expect(User3.type).toBe("User");
+});
+
 test("Read User by Id to return a Single User", function() {
-  // console.log(db);
   expect(db.Users["1"].name).toBe("Uchenna");
 });
 
 test("Database has values and not empty", function() {
-  expect(Object.keys(db.Users).length).toBeGreaterThan(1);
+  expect(Object.keys(db.Users).length).toBeGreaterThan(3);
 });
 
-test("Admin type in database to be 2", function() {
+test('Search User by name to return User Object', function() {
+  expect(User2.searchUserByName('Uche')).toEqual(db.Users['3']);
+});
+
+test('Search Admin by name to return Admin Object', function() {
+  expect(Admin1.searchUserByName('Matthew')).toEqual(db.Users['2']);
+});
+
+test("Admin type of in database to be 2", function() {
   var adminCount = [];
   for (var value in db.Users) {
     if (db.Users[value].type === "Admin") {
       adminCount.push(db.Users[value]);
     }
   }
-  expect(adminCount.length).toBe(2);
+  expect(adminCount.length).toBeGreaterThanOrEqual(2);
 });
 
 test("All single user details fields to be updated", function() {
-  expect(
-    User1.updateSingleUser("Uche", "egbouchenna001@gmail.com", 12345)
-  ).toBe("All fields are required");
+  expect(User1.updateSingleUser("Uche", "egbouchenna001@gmail.com", 12345)).toBe("All fields are required");
 });
 
 test("A single Admin details to be updated", function() {
   expect(
-    Admin2.updateSingleUser(4, "Muse", "handsumMuse@gmail.com", 77889)
+    Admin2.updateSingleUser(4, "MUSE", "handsumMuse@gmail.com", 77889)
   ).toBe("User updated successfully");
 });
-
+console.log(db.Users);
 test("Delete a single user by Admin not in database", function() {
-  console.log(db);
-  expect(Admin1.deleteUser(6)).toBe("User not found");
+  expect(Admin1.deleteUser(8)).toBe("User not found");
 });
 
 test("Delete a single user by Admin", function() {
@@ -68,5 +77,6 @@ test("Delete a single user by Admin", function() {
 });
 
 test("Delete All Users in database", function() {
-  expect(Admin2.deleteAllUsers()).toBe("All Users deleted successfully");
+  expect(Admin3.deleteAllUsers()).toBe("All Users deleted successfully");
 });
+
