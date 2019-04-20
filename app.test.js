29eaf0314625630db2583733772e2b2d6948ad9b
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-20 18:50:34 +0100

    Created orders and Added test to check if orders database contains values

--- app.test.js
+++ app.test.js
@@ -1,12 +1,25 @@
 const { User, Admin } = require("./app");
+const {Orders}  = require('./orders');
 const db = require("./database");
 
+
+// ************************** CREATE  USERS   *******************************
 var User1 = new User("Uchenna", "egbouchenna001@gmail.com", 12345);
 var Admin1 = new Admin("Matthew", "Uchesco4jesus@gmail.com", 54321);
 var User2 = new User("Uche", "uche4ma2w@gmail.com", 99887);
 var Admin2 = new Admin("Muse", "handsum-Muse@gmail.com", "334562");
 var User3 = new User("deleteUser", "deletedUser@gmail.com", "12345");
 var Admin3 = new Admin("Stephan", "rickraggardy@gmail.com", 28290);
+
+// ++++++++++++++++ CREATE ORDERS ++++++++++++++++++++
+
+var Order1 = new Orders(User1.id,'Beans');
+var Order2 = new Orders(User2.id,'Rice');
+var Order3 = new Orders(User2.id,'Plantain');
+var Order4 = new Orders(Admin1.id,'Dog meat');
+var Order5 = new Orders(Admin2.id,'Yam');
+var Order6 = new Orders(Admin3.id,'Garri and Soup');
+
 
 test("User name to be name of the User", function() {
   expect(User1.name).toBe("Uchenna");
@@ -67,7 +80,7 @@ test("A single Admin details to be updated", function() {
     Admin2.updateSingleUser(4, "MUSE", "handsumMuse@gmail.com", 77889)
   ).toBe("User updated successfully");
 });
-console.log(db.Users);
+
 test("Delete a single user by Admin not in database", function() {
   expect(Admin1.deleteUser(8)).toBe("User not found");
 });
@@ -80,3 +93,7 @@ test("Delete All Users in database", function() {
   expect(Admin3.deleteAllUsers()).toBe("All Users deleted successfully");
 });
 
+
+test("Orders Database has values and not empty", function() {
+  expect(Object.keys(db.Orders).length).toBeGreaterThan(3);
+});
