230673e0a33b3856c1a67f0be1aee72be7835ae6
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-20 18:52:58 +0100

    Created the orders constructor to add values into the orders data base

---
+++ orders.js
@@ -0,0 +1,37 @@
+const db = require("./database");
+const app = require("./app");
+
+//      ***************************     CREATE ORDER   ********************
+function Orders(user_id, products) {
+  this.user_id = user_id;
+  this.products = products;
+
+  var time = new Date();
+  this.timeOfOrder = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+
+  var today = new Date();
+  this.dateOfOrder = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+  for (var i = 1; i >= 0; ) {
+    if (!db.Orders[i + ""]) {
+      id = i + "";
+      db.Orders[id] = {
+        user_id: this.user_id,
+        timeOfOrder: this.timeOfOrder,
+        dateOfOrder: this.dateOfOrder,
+        id: id,
+        products: this.products
+      };
+      break;
+    }
+    i++;
+  }
+  this.id = id;
+
+  console.log(db);
+
+}
+
+
+
+module.exports = { Orders };
