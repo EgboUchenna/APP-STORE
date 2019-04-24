b297356b6d95a215f7fea1e33c6210fdc0310db3
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-24 21:03:54 +0100

    Refactored tests to check if User can output a single user.

--- User.test.js
+++ User.test.js
@@ -1,6 +1,7 @@
 const Admin = require("./Admin");
 const User = require("./User");
 const Order = require("./orders");
+const db = require('./database');
 
 
 var user1 = new User("Uche", "Uche@gmail.com", "password");
@@ -9,11 +10,15 @@ var user3 = new User("Muse", "Muse@gmail.com", "jsdknsUqd");
 var user4 = new User("Steph", "Steph@gmail.com", "nsnklslRE");
 
 test("User can create new Order", function() {
-    var newOrder = user1.createNewOrder(["Rice", "Beans"]);
-    expect(newOrder).toBeTruthy();
+    var newOrder1 = user4.createNewOrder(["Banners", "Flyers", "Stickers"]);
+    expect(newOrder1).toBeTruthy();
   });
   
 
 test("User cannot delete  Order", function() {
     expect(user1.deleteOrder(1)).toBeFalsy();
+  });
+
+  test('User to output single user object', function() {
+      expect(user2.readSingleUser(3)).toEqual(db.Users['3']);
   });
