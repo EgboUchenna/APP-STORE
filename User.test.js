ca8484fea4925f8a535611e0455c897ceb1f13d4
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-04-24 23:00:35 +0100

    Refactored test to check user cannot read order by id.

--- User.test.js
+++ User.test.js
@@ -1,8 +1,7 @@
 const Admin = require("./Admin");
 const User = require("./User");
 const Order = require("./orders");
-const db = require('./database');
-
+const db = require("./database");
 
 var user1 = new User("Uche", "Uche@gmail.com", "password");
 var user2 = new User("Matt", "Matt@gmail.com", "ggdhrwR");
@@ -10,15 +9,18 @@ var user3 = new User("Muse", "Muse@gmail.com", "jsdknsUqd");
 var user4 = new User("Steph", "Steph@gmail.com", "nsnklslRE");
 
 test("User can create new Order", function() {
-    var newOrder1 = user4.createNewOrder(["Banners", "Flyers", "Stickers"]);
-    expect(newOrder1).toBeTruthy();
-  });
-  
+  var newOrder1 = user4.createNewOrder(["Banners", "Flyers", "Stickers"]);
+  expect(newOrder1).toBeTruthy();
+});
 
 test("User cannot delete  Order", function() {
-    expect(user1.deleteOrder(1)).toBeFalsy();
-  });
+  expect(user1.deleteOrder(1)).toBeFalsy();
+});
 
-  test('User to output single user object', function() {
-      expect(user2.readSingleUser(3)).toEqual(db.Users['3']);
-  });
+test("User to output single user object", function() {
+  expect(user2.readSingleUser(3)).toEqual(db.Users["3"]);
+});
+
+test("User cannot read one order by id", function() {
+  expect(user3.readOneOrderById(3)).toBeFalsy();
+});
